fc5f388143e3fe3761f543c23cf1139f80bb3e6d
jnfviana <joao_nsviana@hotmail.com>
2020-04-16 10:59:35 +0100

    Lesson 39 commit

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+
+		System.out.println("--- Profiles ---");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+
+		System.out.println(i18nController.sayHello());
+
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		System.out.println("--- Primary Bean ---");
+
+		System.out.println(myController.sayHello());
+
+		System.out.println("--- Property Injection ---");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--- Setter Injection ---");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("--- Constructor ---");
+
+		ConstructerInjectedController constructerInjectedController = (ConstructerInjectedController) ctx.getBean("constructerInjectedController");
+
+		System.out.println(constructerInjectedController.getGreeting());
+
+	}
+
+}
---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructerInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructerInjectedController {
+
+    private final GreetingService greetingService;
+
+    // The autowired is not necessary for constructers.
+    public ConstructerInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}

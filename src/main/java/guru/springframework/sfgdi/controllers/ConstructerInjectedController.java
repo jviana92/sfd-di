f5a1544ef7ab7d358d853e9ab8de948e5b4f82a6
jnfviana <joao_nsviana@hotmail.com>
2020-04-23 18:26:55 +0100

    Component Scans example

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructerInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructerInjectedController.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import guru.services.GreetingService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
@@ -10,7 +9,7 @@ public class ConstructerInjectedController {
 
     private final GreetingService greetingService;
 
-    // The autowired is not necessary for constructers.
+    // The autowired is not necessary for constructors.
     public ConstructerInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
